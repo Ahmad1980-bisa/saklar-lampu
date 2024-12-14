d8f979e6a9958d24eabb8300e4e7df74645157e4
Ahmad1980-bisa <ahmad.damanik980@gmail.com>
2024-12-14 23:38:17 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("on").onclick = function() {
+		 	document.getElementById("lampu").src = "images/nyala.png";
+		 }
+
+		 document.getElementById("off").onclick = function() {
+		 	document.getElementById("lampu").src = "images/mati.png";
+		 }
